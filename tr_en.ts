ff71cc6844693acc101b6f62438c74899f76eb5c
Inori Sakura <inorindesu@gmail.com>
2013-02-17 01:28:16 +0800

    Completed current translation.

--- tr_en.ts
+++ tr_en.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="MainWindow.cpp" line="43"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>Ready</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="170"/>
         <source>Exit game?</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit game?</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="170"/>
         <source>A game is still in progress. Do you want to exit?</source>
-        <translation type="unfinished"></translation>
+        <translation>A game is still in progress. Do you want to exit?</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="448"/>
         <source>Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Game</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="449"/>
         <source>New Game</source>
-        <translation type="unfinished"></translation>
+        <translation>New Game</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="450"/>
         <source>Stop Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Stop Game</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="451"/>
         <source>Pause Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Pause Game</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="452"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="453"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
 </context>
 </TS>
--- tr_zh.ts
+++ tr_zh.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="MainWindow.cpp" line="43"/>
         <source>Ready</source>
-        <translation type="unfinished"></translation>
+        <translation>就緒</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="170"/>
         <source>Exit game?</source>
-        <translation type="unfinished"></translation>
+        <translation>離開遊戲？</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="170"/>
         <source>A game is still in progress. Do you want to exit?</source>
-        <translation type="unfinished"></translation>
+        <translation>遊戲正在進行中。請問您要離開嗎？</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="448"/>
         <source>Game</source>
-        <translation type="unfinished"></translation>
+        <translation>遊戲</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="449"/>
         <source>New Game</source>
-        <translation type="unfinished"></translation>
+        <translation>新遊戲</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="450"/>
         <source>Stop Game</source>
-        <translation type="unfinished"></translation>
+        <translation>停止遊戲</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="451"/>
         <source>Pause Game</source>
-        <translation type="unfinished"></translation>
+        <translation>暫停遊戲</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="452"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>離開程式</translation>
     </message>
     <message>
         <location filename="MainWindow.cpp" line="453"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>設定</translation>
     </message>
 </context>
 </TS>
